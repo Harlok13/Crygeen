933fa2f1e7a0056b4cdc83dde5663059dcb5c772
harlok <holmik90@yandex.ru>
2023-05-10 13:25:33 +0400

    ref(saver.py): use Path instead os.path

--- crygeen/saver.py
+++ crygeen/saver.py
@@ -1,6 +1,7 @@
 import json
 import os
 from pathlib import Path
+from typing import NamedTuple
 
 import pygame as pg
 from pydantic import FilePath
@@ -14,34 +15,41 @@ class SaveLoadManager:
 
         self.save_load_base_path: str = settings.SAVE_LOAD_BASE_PATH
 
-        # self.control_data_path: str = settings.CONTROL_DATA_PATH
-        # self.name_control_file: str = settings.NAME_CONTROL_FILE
+        self.control_data_path: Path = settings.CONTROL_DATA_PATH
 
-    def write_save(self, data: dict, file_path: FilePath) -> None:
-        """
-        The function writes the data to a file at the specified path. The data is
-        written in JSON format.
-        :param data: The data to write to the file. It should be a dictionary.
-        :param file_path: The path to the file where the data will be written. This should
-                        be a 'PathLike'.
-        :return:
-        """
-        file_path = Path(file_path) if not isinstance(file_path, Path) else file_path
-        with open(os.path.join(self.save_load_base_path, file_path), 'w') as file:
-            json.dump(data, file)
 
-    @staticmethod
-    def __load_existing_save(save_file) -> dict:
-        with open(os.path.join(save_file), 'r+') as file:
-            data = json.load(file)
-        return data
+def write_save(self, data, file_path: FilePath | Path) -> None:
+    """
+    The function writes the data to a file at the specified path. The data is
+    written in JSON format.
+    :param data: The data to write to the file. It should be a dictionary.
+    :param file_path: The path to the file where the data will be written. This should
+                    be a 'PathLike'.
+    :return:
+    """
+    file_path: Path = self.__check_file_path(file_path)
+    with open(file_path, 'w') as file:
+        json.dump(data, file)
 
-    def load_save(self, save) -> dict:
-        try:
-            save: dict = self.__load_existing_save(save)
-        except FileNotFoundError as exc:
-            raise exc  # TODO handle exception
-        return save
+
+def __load_existing_save(self, save_file) -> dict | list[list[str, int, str]]:
+    save_file: Path = self.__check_file_path(save_file)
+    with open(save_file, 'r+') as file:
+        data = json.load(file)
+    return data
+
+
+@staticmethod
+def __check_file_path(file_path: FilePath) -> Path:
+    return Path(file_path) if not isinstance(file_path, Path) else file_path
+
+
+def load_save(self, save: FilePath) -> dict | list[list[str, int, str]]:
+    try:
+        save: dict | list[str, int, str] = self.__load_existing_save(save)
+    except FileNotFoundError as exc:
+        raise exc  # TODO handle exception
+    return save
 
     def create_control_save(self):
         pass
