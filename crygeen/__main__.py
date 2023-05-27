74ab59411254686ef0f804a9a5dc5dcf6e9b1f29
harlok <holmik90@yandex.ru>
2023-05-28 02:18:06 +0400

    fix(exit_menu.py): fix sound and add camera offset

--- crygeen/__main__.py
+++ crygeen/__main__.py
@@ -5,22 +5,22 @@ from typing import Optional
 import pygame as pg
 from pygame import QUIT, KEYDOWN, KEYUP, Surface, MOUSEBUTTONDOWN, MOUSEWHEEL
 from pygame.event import Event
+from pygame.mixer import Sound
 from pygame.time import Clock
 
 from crygeen import audio
-from crygeen.buttons import ControlButton, Button
+from crygeen.main_menu.buttons import ControlButton, Button
 from crygeen.game_process.game_settings import gSettings
 from crygeen.game_process.level import Level
 from crygeen.game_process.spritesheet import SpriteSheet
-from crygeen.menu import Menu, ExitMenu, ScreensaverMenu, SettingsMenu
-from crygeen.particles import ParticlePlayer
+from crygeen.main_menu.menu_categories.exit_menu import ExitMenu
+from crygeen.main_menu.menu_categories.menu import Menu
+from crygeen.main_menu.particles import ParticlePlayer
+from crygeen.main_menu.menu_categories.screensaver_menu import ScreensaverMenu
+from crygeen.main_menu.menu_categories.settings_menu import SettingsMenu
 
 from crygeen.settings import settings
-from crygeen.states import Status, State
-
-import pygame.gfxdraw
-
-from crygeen.utils.debug import debug
+from crygeen.main_menu.states import Status, State
 
 
 class EventHandler:
@@ -30,11 +30,11 @@ class EventHandler:
         self.select_button: Optional[ControlButton] = None
 
     @staticmethod
-    def __close_game():
+    def __close_game() -> None:
         pg.quit()
         exit()
 
-    def __click_button(self, buttons_list):
+    def __click_button(self, buttons_list: list[Button]) -> None:
         for button in buttons_list:
             if button.rect.collidepoint(pg.mouse.get_pos()):
                 if button.control_button:
@@ -42,16 +42,16 @@ class EventHandler:
                     self.select_button: ControlButton = button.control_button
                 self.game.status = button.input(self.game)
 
-    def __screensaver_event_handler(self, event: Event):
+    def __screensaver_event_handler(self, event: Event) -> None:
         if event.type == pg.KEYDOWN:
             self.game.status = Status.MAIN_MENU
             self.game.menu.dropdown_start_time = pg.time.get_ticks()
 
-    def __main_menu_event_handler(self, event: Event):
+    def __main_menu_event_handler(self, event: Event) -> None:
         if event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
             self.__click_button(self.game.menu.menu_buttons_list)
 
-    def __exit_menu_event_handler(self, event: Event):
+    def __exit_menu_event_handler(self, event: Event) -> None:
         if event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
             self.__click_button(self.game.exit_menu.exit_buttons_list)
 
@@ -62,7 +62,7 @@ class EventHandler:
             elif event.key == pg.K_RETURN:
                 self.__close_game()
 
-    def __settings_menu_event_handler(self, event: Event):
+    def __settings_menu_event_handler(self, event: Event) -> None:
         if event.type == pg.MOUSEBUTTONDOWN:
             if event.button == 1:
                 self.__click_button(self.game.settings_menu.control_buttons_list)
@@ -72,7 +72,7 @@ class EventHandler:
         elif event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
             self.game.status = Status.MAIN_MENU
 
-    def __control_menu_event_handler(self, event):
+    def __control_menu_event_handler(self, event: Event) -> None:
         if event.type == pg.KEYDOWN:
             if event.key == pg.K_ESCAPE:
                 self.game.status = Status.SETTINGS
@@ -80,14 +80,14 @@ class EventHandler:
                 self.select_button.set_new_key(event, game)
                 self.select_button = None
 
-    def event_loop(self):
+    def event_loop(self) -> None:
         for event in pg.event.get():
-            self.game.level.player.keyboard_input(event)
+            self.game.level.player.keyboard_input.keyboard_input(event)
             match event.type:
                 case pg.QUIT:
                     self.__close_game()
 
-            match self.game.status:
+            match self.game.status:  # todo menu event handler class
                 case Status.SCREENSAVER:
                     self.__screensaver_event_handler(event)
                 case Status.MAIN_MENU:
@@ -110,23 +110,24 @@ class Game:
             settings.FLAGS,
             settings.BIT_PER_PIXEL
         )
-        pg.display.set_icon(pg.image.load(settings.GAME_ICO))
+        # pg.display.set_icon(pg.image.load(settings.GAME_ICO))
         pg.display.set_caption(settings.GAME_TITLE)
         self.clock: Clock = pg.time.Clock()
 
         self.event_handler: EventHandler = handler
         self.event_handler.game = self
 
-        self.state: State = State.MAIN_MENU
-        # self.state: State = State.GAME
+        # self.state: State = State.MAIN_MENU
+        self.state: State = State.GAME
 
         # menu setup ________________________________________________________________________________
         self.status: Status = Status.SCREENSAVER
 
-        self.main_sound = pg.mixer.Sound(random.choice(audio.MAIN_MENU_SOUND))
+        self.main_sound: Sound = pg.mixer.Sound(random.choice(audio.MAIN_MENU_SOUND))
         self.main_sound.set_volume(audio.MAIN_MENU_VOLUME)
         self.toggle_music_flag: bool = True
 
+        # initialize menu
         self.menu: Menu = Menu()
         self.exit_menu: ExitMenu = ExitMenu()
         self.settings_menu: SettingsMenu = SettingsMenu()
@@ -145,23 +146,21 @@ class Game:
 
         self.game_fps: int = gSettings.GAME_FPS
 
-        pg.event.set_allowed([QUIT, KEYDOWN, KEYUP, MOUSEBUTTONDOWN, MOUSEWHEEL])
-
-        self.previous_time: float = time.time()
-
         self.sprite_sheet: SpriteSheet = SpriteSheet(
             gSettings.PLAYER_SPRITE_SHEET_PATH, gSettings.PLAYER_SPRITE_METADATA_PATH
         )
+        pg.event.set_allowed([QUIT, KEYDOWN, KEYUP, MOUSEBUTTONDOWN, MOUSEWHEEL])
 
+        self.previous_time: float = time.time()
         self.level: Level = Level(self, self.sprite_sheet)
 
 
-    def simple_toggle_music(self):
+    def simple_toggle_music(self) -> None:
         if self.toggle_music_flag:
             self.main_sound.play(loops=audio.MAIN_MENU_LOOPS)
             self.toggle_music_flag: bool = False
 
-    def run_menu(self, status):
+    def run_menu(self, status: Status) -> None:
         self.screensaver_menu.start_screensaver(status)
         # self.simple_toggle_music()
         if status != Status.SCREENSAVER:
@@ -171,20 +170,22 @@ class Game:
         elif status == Status.SETTINGS or self.status == Status.SET_CONTROL:
             self.settings_menu.display_settings_menu(status)
 
-    def run(self):
+    def run(self) -> None:
         while True:
             dt: float = time.time() - self.previous_time
             self.previous_time: float = time.time()
 
-            self.event_handler.event_loop()  # unfortunately it must be first
             match self.state:
                 case State.MAIN_MENU:
                     self.run_menu(self.status)
                 case State.GAME:
                     self.level.run(dt)
-                    self.screen.blit(self.level.game_canvas, (0, 0))
+                    self.screen.blit(self.level.game_canvas, (
+                        self.level.player.keyboard_input.camera_x,
+                        self.level.player.keyboard_input.camera_y
+                    ))  # todo ref
 
-
+            self.event_handler.event_loop()
             # debug(self.level.player.status, 1000, 20)
             pg.display.update()
             self.clock.tick(self.menu_fps)
