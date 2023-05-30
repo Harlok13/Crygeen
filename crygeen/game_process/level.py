6faae59b2d7da727216972cca65996e80e09bf6b
harlok <holmik90@yandex.ru>
2023-05-30 16:43:54 +0400

    feat(level.py): include Grass

--- crygeen/game_process/level.py
+++ crygeen/game_process/level.py
@@ -1,27 +1,50 @@
 import pygame as pg
 from pygame import Surface
 
-from crygeen.game_process.player import Player
+from crygeen.game_process.enemy import Enemy
+from crygeen.game_process.grass_setup.grass_api import Grass
+from crygeen.game_process.magic_setup.lightning_setup import Lightning
+from crygeen.game_process.player_setup.player import Player
+from crygeen.game_process.spritesheet import SpriteSheet
+from crygeen.game_process.particles import ParticlePlayer
 
 
 class Level:
-    def __init__(self, game, sprite_sheet):  # type: ('Game', 'SpriteSheet') -> None
+    def __init__(self, game, sprite_sheet):  # type: ('Game', SpriteSheet) -> None
         # general setup _____________________________________________________________________________
         self.game_canvas: Surface = game.game_canvas
         self.game_paused: bool = False
 
-        self.sprite_sheet = sprite_sheet
+        self.sprite_sheet: SpriteSheet = sprite_sheet
 
         # sprite group setup ________________________________________________________________________
         self.visible_sprites = YSortCameraGroup()
         self.obstacle_sprites = pg.sprite.Group()
 
-        self.player = Player([self.visible_sprites], self.obstacle_sprites, self.sprite_sheet)
+        self.grass: Grass = Grass()
 
-    def run(self, dt):
+        # player setup ______________________________________________________________________________
+        self.player: Player = Player([self.visible_sprites], self.obstacle_sprites, self.sprite_sheet)
+
+        # magic
+        self.lightning: Lightning = Lightning(self.game_canvas)
+
+        self.particle_player: ParticlePlayer = ParticlePlayer(self.game_canvas)
+
+        self.enemy: Enemy = Enemy([self.visible_sprites], self.obstacle_sprites, self.player, self.particle_player)
+
+    def run(self, dt: float) -> None:
+        self.game_canvas.fill((27, 66, 52))
         self.player.update(dt)
-        self.game_canvas.fill('white')
+        self.grass.render(dt, self.game_canvas, self.player)
         self.game_canvas.blit(self.player.image, self.player.rect)
+        self.particle_player.update(pg.math.Vector2(600, 400))
+
+        if self.player.keyboard_input.lightning:
+            self.lightning.update(self.player.rect.center)
+
+        self.enemy.render(self.game_canvas)
+
 
 
 class YSortCameraGroup(pg.sprite.Group):
