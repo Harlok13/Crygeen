0021f0f0c4151fbf2a52ccfe2c4f2f461e618fca
harlok <holmik90@yandex.ru>
2023-05-30 16:52:43 +0400

    fix(player_animation.py): fix properties

--- crygeen/game_process/player_setup/player_animation.py
+++ crygeen/game_process/player_setup/player_animation.py
@@ -13,11 +13,11 @@ class PlayerAnimation:
     def play_animation(self, dt: float, status: str) -> None:
         self._frame_index += self._animation_speed * dt
 
-        if self._frame_index >= self.player._sprite_sheet.data['frames'][f'{status}0.png']['animation_len']:
+        if self._frame_index >= self.player.sprite_sheet.data['frames'][f'{status}0.png']['animation_len']:
             self._frame_index: int = 0
-        sprite: Surface = self.player._sprite_sheet.parse_sprite(f'{status}{int(self._frame_index)}.png')
+        sprite: Surface = self.player.sprite_sheet.parse_sprite(f'{status}{int(self._frame_index)}.png')
 
-        if self.player._sprite_sheet.data['frames'][f'{status}0.png'].get('reverse', False):
+        if self.player.sprite_sheet.data['frames'][f'{status}0.png'].get('reverse', False):
             sprite: Surface = pg.transform.flip(sprite, True, False)
 
         self.player.image = sprite
