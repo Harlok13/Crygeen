eb674bd3dec0afaaca9ae51cd7e58ba0f82af95f
harlok <holmik90@yandex.ru>
2023-05-30 16:48:50 +0400

    ref(menu.py): ref Menu

--- crygeen/main_menu/menu_categories/menu.py
+++ crygeen/main_menu/menu_categories/menu.py
@@ -8,18 +8,18 @@ from pygame.font import Font
 
 from crygeen.main_menu.buttons import Button
 from crygeen.main_menu.saver import SaveLoadManager
-from crygeen.settings import settings
 from crygeen.main_menu.states import Status
+from crygeen.settings import settings
 
 
 class Menu:
     def __init__(self) -> None:
         # general setup _____________________________________________________________________________
-        self._display_surface: Surface = pg.display.get_surface()
-        self._screen_size: tuple[int, int] = self._display_surface.get_size()
+        self.display_surface: Surface = pg.display.get_surface()
+        self.screen_size: tuple[int, int] = self.display_surface.get_size()
         self.save_load_manager: SaveLoadManager = SaveLoadManager()
 
-        # menu setup ________________________________________________________________________________
+        # main_menu setup ________________________________________________________________________________
         self.main_menu_font_name: str = settings.MAIN_MENU_FONT
         self.main_menu_font_size: int = settings.MAIN_MENU_FONT_SIZE
         self.main_menu_font_color: tuple[int, int, int] = settings.MAIN_MENU_FONT_COLOR
@@ -28,12 +28,12 @@ class Menu:
         self.alpha: int = settings.MAIN_MENU_ALPHA
         self.button_opacity_offset: float = settings.MAIN_MENU_BUTTON_OPACITY_OFFSET
 
-        # menu buttons setup
+        # main_menu buttons setup
         self.menu_buttons_list: list = []
         self._menu_y_dest_positions: list = []
         self.__create_menu_buttons()
 
-        # dropdown menu effect
+        # dropdown main_menu effect
         self.dropdown_animation_time: int = settings.MAIN_MENU_DROPDOWN_ANIMATION
         self.dropdown_start_time: int = 0
 
@@ -41,8 +41,10 @@ class Menu:
         self.img = pg.image.load(
             settings.BASE_PATH.joinpath('assets', 'graphics', 'settings', 'setting_bg6.jpeg')).convert_alpha()
 
+        self.close_exit_menu: bool = False
+
     # supporting functions __________________________________________________________________________
-    def _alpha_vanish(
+    def alpha_vanish(
             self,
             duration: int,
             start_time: int,
@@ -71,32 +73,30 @@ class Menu:
         erp_alpha: float = self._lerp(start_alpha, end_alpha, delta / duration)
         if op(erp_alpha, end_alpha):  # < or >
             surface.set_alpha(erp_alpha)  # type: ignore
-        self._display_surface.blit(surface, (0, 0))
+        self.display_surface.blit(surface, (0, 0))
 
-    def _dropdown_menu_effect(
+    def dropdown_menu_effect(
             self,
             buttons_list: list[Button],
             start_time: int,
-            animation_time: int,
+            animation_duration: int,
             y_dest_positions: list[int],
             shading: bool = False
     ) -> None:
         """
-        Play animation, when open menu.
-        self.dropdown_animation_time: The duration of the animation.
-        delta: The time elapsed since the function was called.
-        :return:
+        Play animation, when open main_menu.
+        :return: None
         """
         opacity_values = (self.alpha, 0) if shading else (0, self.alpha)
-        if animation_time > 0:
+        if animation_duration > 0:
             delta: int = pg.time.get_ticks() - start_time
             for button in buttons_list:
-                if delta < animation_time:
+                if delta < animation_duration:
                     button.rect.y = self._lerp(
-                        button.rect.y, y_dest_positions[button.index], delta / animation_time
+                        button.rect.y, y_dest_positions[button.index], delta / animation_duration
                     )
                     button.surf.set_alpha(self._lerp(
-                        *opacity_values, delta / animation_time
+                        *opacity_values, delta / animation_duration
                     ))
 
     @staticmethod
@@ -114,33 +114,30 @@ class Menu:
         return a + (b - a) * t
 
     @staticmethod
-    def _add_text_effect(obj: Surface):
+    def _add_text_effect(obj: Surface) -> None:
         item_alpha: int = int(abs(math.sin(pg.time.get_ticks() / 1000)) * 128 + 128)
         obj.set_alpha(item_alpha)
 
-    def _draw_text(self, text: str, font: Font, x: int, y: int) -> tuple[Rect, Surface]:
+    def draw_text(self, text: str, font: Font, x: int, y: int) -> tuple[Surface, Rect]:
         """
         Draw a text on the screen.
         :param text: The text to be displayed.
         :param font: The font used for the text.
-        :param surface: The Surface object on which the text will be drawn.
         :param x: The x-coordinate of the starting point of text display.
         :param y: The y-coordinate of the starting point of text display.
-        :param emergence: If the text should be smoothly emerging.
-        :param text_effect: If the text should be animated.
         :return:
         """
-        text_obj: Surface = font.render(text, True, self.main_menu_font_color)
-        text_rect: Rect = text_obj.get_rect()
+        text_surf: Surface = font.render(text, True, self.main_menu_font_color)
+        text_rect: Rect = text_surf.get_rect()
         text_rect.center = (x, y)
 
-        return text_rect, text_obj
+        return text_surf, text_rect,
 
-    # main menu setup _______________________________________________________________________________
+    # main main_menu setup _______________________________________________________________________________
     def __create_menu_buttons(self) -> None:
         """
-        Creates the main menu buttons and add them to the buttons list.
-        Сoords are set for the location of buttons on the screen.
+        Creates the main main_menu buttons and add them to the buttons list.
+        Coordinates are set for the location of buttons on the screen.
         :return:
         """
         x: int = settings.MAIN_MENU_X
@@ -164,27 +161,37 @@ class Menu:
             self._menu_y_dest_positions.append(dest_pos_y)
             self.menu_buttons_list.append(button)
 
-    def display_menu(self, status: Status, exit_menu, settings_menu):
+    def display_menu(self, status, exit_menu, settings_menu):  # type: (Status, 'ExitMenu', 'SettingsMenu') -> None
         """
-        Displays menu buttons on the screen based on the application state.
-        If the application status is 'Status.MAIN_MENU', the method starts the menu
-        animation and displays all menu buttons. If the mouse is hovering
+        Displays main_menu buttons on the screen based on the application state.
+        If the application status is 'Status.MAIN_MENU', the method starts the main_menu
+        animation and displays all main_menu buttons. If the mouse is hovering
         over a button, it "fades in" using the 'fade_in_hover' method,
         otherwise it returns to its original value.
         :return:
         """
         if status == Status.MAIN_MENU:
-            self._dropdown_menu_effect(
+            self.dropdown_menu_effect(
                 self.menu_buttons_list, self.dropdown_start_time, self.dropdown_animation_time,
                 self._menu_y_dest_positions
             )
+
         if status != Status.MAIN_MENU:
-            self._dropdown_menu_effect(
-                self.menu_buttons_list, exit_menu.exit_dropdown_start_time, self.dropdown_animation_time,
+            self.dropdown_menu_effect(
+                self.menu_buttons_list, self.dropdown_start_time, self.dropdown_animation_time,
                 [1000] * len(self.menu_buttons_list), True
             )  # TODO ref hard code
+
+        if self.close_exit_menu:
+            self.dropdown_menu_effect(
+                exit_menu.exit_buttons_list, exit_menu.exit_dropdown_start_time, exit_menu.exit_dropdown_duration,
+                [1000] * len(exit_menu.exit_buttons_list), True
+            )
+            for button in exit_menu.exit_buttons_list:
+                self.display_surface.blit(button.surf, button.rect)
+            # close_exit_menu = False
 
         for button in self.menu_buttons_list:
             button.fade_in_hover()
 
-            self._display_surface.blit(button.surf, button.rect)
+            self.display_surface.blit(button.surf, button.rect)
