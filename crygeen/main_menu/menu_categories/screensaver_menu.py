5853a2631aed30feb534f2ac47467b26e36e5c1e
harlok <holmik90@yandex.ru>
2023-06-01 19:52:10 +0400

    feat(screensaver_menu.py): add text properties

--- crygeen/main_menu/menu_categories/screensaver_menu.py
+++ crygeen/main_menu/menu_categories/screensaver_menu.py
@@ -13,17 +13,22 @@ from crygeen.utils.support import import_folder_img
 
 class ScreensaverMenu:
     def __init__(self):
+        # general setup
         self.screen_size: tuple[int, int] = pg.display.get_window_size()
+
         # bg opacity effect
         self.flag: bool = False
         self.fade_surf: Surface = pg.Surface(self.screen_size)
         self.fade_surf.set_alpha(255)
         self.dropdown_start_time: int = 0
         self.alpha_vanish_duration: int = settings.SCREENSAVER_ALPHA_VANISH_DURATION
-        self.start_alpha_vanish: int = settings.SCREENSAVER_START_ALPHA_VANISH
+        self.start_alpha_vanish_opacity: int = settings.SCREENSAVER_START_ALPHA_VANISH_OPACITY
+        self.end_alpha_vanish_opacity: int = settings.SCREENSAVER_END_ALPHA_VANISH_OPACITY
         # text opacity effect
-        self.alpha_text_duration: int = settings.SCREENSAVER_ALPHA_TEXT_DURATION
-        self.start_text_alpha: tuple[int, int] = settings.SCREENSAVER_START_TEXT_ALPHA
+        self.text_alpha_duration: int = settings.SCREENSAVER_TEXT_ALPHA_DURATION
+        self.text_alpha: tuple[int, int] = settings.SCREENSAVER_TEXT_ALPHA
+        self.text_end_alpha: int = settings.SCREENSAVER_TEXT_END_ALPHA
+        self.animation_start_time: int = 0
 
         # screensaver bg
         self.bg_path: Path = settings.SCREENSAVER_PATH
@@ -45,33 +50,8 @@ class ScreensaverMenu:
         """
         screensaver_data: list[Surface] = import_folder_img(self.bg_path)
 
-        screensaver_scale_data: list[Surface] = [pg.transform.scale(item, (1280, 800)) for item in
-                                                 screensaver_data]  # TODO ref hard code
+        screensaver_scale_data: list[Surface] = [
+            pg.transform.scale(item, self.screen_size) for item in screensaver_data
+        ]
 
         return screensaver_scale_data
-
-    @staticmethod
-    def screensaver_text_effect(alpha_values: tuple[int, int], emergence_duration: int, start_time: int) -> Callable:
-        def outer_wrapper(func: Callable) -> Callable:
-            @wraps(func)
-            def wrapper(*args, **kwargs) -> tuple[Surface, Rect]:
-
-                text_surf, text_rect = func(*args, **kwargs)
-
-                delta: int = pg.time.get_ticks() - start_time
-                start_alpha, end_alpha = alpha_values
-                dt: float = delta / emergence_duration
-                alpha_offset: float | int = start_alpha + (end_alpha - start_alpha) * dt
-
-                if alpha_offset <= 200:
-                    text_surf.set_alpha(alpha_offset)
-                else:
-                    wave_alpha: int = int(abs(math.sin(pg.time.get_ticks() / 1000)) * 128 + 128)
-                    text_surf.set_alpha(wave_alpha)
-
-                return text_surf, text_rect
-
-            return wrapper
-
-        return outer_wrapper
-
