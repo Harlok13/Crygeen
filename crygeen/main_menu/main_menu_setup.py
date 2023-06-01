87193a7a53bfe2c60d4c16c15d7e57534ca6ee60
harlok <holmik90@yandex.ru>
2023-06-01 20:04:13 +0400

    ref(main_menu_setup.py): ref MainMenuSetup

--- crygeen/main_menu/main_menu_setup.py
+++ crygeen/main_menu/main_menu_setup.py
@@ -6,7 +6,7 @@ from pygame.mixer import Sound
 from pygame import QUIT, KEYDOWN, KEYUP, Surface, MOUSEBUTTONDOWN, MOUSEWHEEL, Rect
 
 from crygeen import audio
-from crygeen.main_menu.menu_animation_player import MenuPlayer
+from crygeen.main_menu.menu_animation.menu_player import MenuPlayer
 from crygeen.main_menu.menu_categories.exit_menu import ExitMenu
 from crygeen.main_menu.menu_categories.menu import Menu
 from crygeen.main_menu.particles import ParticlePlayer
@@ -17,11 +17,13 @@ from crygeen.main_menu.saver import SaveLoadManager
 
 class MainMenuSetup:
     def __init__(self):
+        # general setup
         self.__main_sound: Sound = pg.mixer.Sound(random.choice(audio.MAIN_MENU_SOUND))
         self.__main_sound.set_volume(audio.MAIN_MENU_VOLUME)
         self.toggle_music_flag: bool = True
         self.save_load_manager: SaveLoadManager = SaveLoadManager()
 
+        # init categories
         self.menu: Menu = Menu()
         self.exit_menu: ExitMenu = ExitMenu()
         self.settings_menu: SettingsMenu = SettingsMenu(self.save_load_manager)
@@ -29,8 +31,6 @@ class MainMenuSetup:
         self.menu_player: MenuPlayer = MenuPlayer(
             self.menu, self.exit_menu, self.settings_menu, self.screensaver_menu
         )
-
-        self.particle_player = ParticlePlayer()
 
         self.fps: Optional[int] = None
 
@@ -44,4 +44,3 @@ class MainMenuSetup:
     def run_menu(self) -> None:
         self.__simple_toggle_music()
         self.menu_player.update()
-        self.cursor.update()
