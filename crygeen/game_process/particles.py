87c35f90d90c4ba50b1a8cce7e1fe0412a735ba5
harlok <holmik90@yandex.ru>
2023-06-04 21:20:49 +0400

    feat(particles.py): add ParticleBlood and ParticlePlayer

--- crygeen/game_process/particles.py
+++ crygeen/game_process/particles.py
@@ -3,75 +3,118 @@ import random
 import pygame as pg
 from pygame import Surface, Vector2
 
+from crygeen.game_process.game_settings import gSettings, ParticleDirection, ParticleRadius
 
-class Particle:
-    foo = {(0, -1): {'x': (1, -1), 'y': (-1,-1)},
-           (0, 1): {'x': (1, -1), 'y': (1,1)},
-           (1, 0): {'x': (1,1), 'y': (1, -1)},
-           (-1, 0): {'x': (-1, -1), 'y': (1, -1)},
-           (.7, -.7): {'x': (1, 1), 'y': (-1,-1)},
-           (-.7, -.7): {'x': (-1, -1), 'y': (-1,-1)},
-           (-.7, .7): {'x': (-1, -1), 'y': (1, 1)},
-           (.7, .7): {'x': (1, 1), 'y': (1, 1)}}
 
-    def __init__(self, position: Vector2, radius: int, color: tuple[int, int, int], direction: Vector2) -> None:
-        self.position: Vector2 = position
-        self.radius: int = radius
-        self.color: tuple = color
-        self.direction = Vector2(1, 0)
+class ParticleBlood(pg.sprite.Sprite):
+    """One piece of blood"""
 
-        self.x_offset: float = random.choice(self.foo.get(tuple(direction.xy))['x']) * random.random()
-        self.y_offset: float = random.choice(self.foo.get(tuple(direction.xy))['y']) * random.random() * 4
+    def __init__(
+            self,
+            group,
+            pos_x: int,
+            pos_y: int,
+            radius: ParticleRadius,
+            direction_x: ParticleDirection,
+            direction_y: ParticleDirection,
+            duration: int,
+            start_time: int
+    ) -> None:
+
+        super().__init__(group)
+        self.pos_x: int = pos_x
+        self.pos_y: int = pos_y
+        self.pos_vec: Vector2 = pg.Vector2(self.pos_x, self.pos_y)
+
+        self.radius: int = radius.start_radius
+        self.dest_radius: int = random.randint(radius.start_radius, radius.dest_radius)
+
+        self.direction_x: int = random.randint(*direction_x)
+        self.direction_y: int = random.randint(*direction_y)
+        self.dir_vec: Vector2 = pg.Vector2(self.direction_x, self.direction_y)
+
+        self.dest_vec: Vector2 = pg.Vector2(self.pos_vec + self.dir_vec)
+
+        self.duration: int = duration
+        self.start_time: int = start_time
+
+        self.color: tuple[int, int, int] = random.choice(gSettings.BLOOD_COLORS)
+
+        self.image = pg.Surface((radius.dest_radius * 2, radius.dest_radius * 2)).convert_alpha()
+        self.image.fill('white')
+        self.image.set_colorkey((255, 255, 255))
+
+        self.rect = self.image.get_rect(center=self.pos_vec)
+
+        self.lifetime: int = gSettings.BLOOD_PARTICLE_LIFETIME
 
 
 class ParticlePlayer:
+    def __init__(self, group) -> None:
+        self.particles: list[ParticleBlood] = []
+        self.particles_amount: int = gSettings.BLOOD_PARTICLE_AMOUNT
 
-    def __init__(self, surface: Surface) -> None:
-        self._particles_list: list[Particle] = []
-        self._surface: Surface = surface
-        self.object_pos: Vector2 = pg.math.Vector2()
-        self.__create_particles_list(100, Vector2(300, 400), 4, (255, 255, 255))
+        self.group = group
 
-        # self._particles_list: list[Particle] = [Particle(Vector2(600, 400), 4, (255, 0, 0), Vector2(1, 0)) for i in range(50)]
-        self._hit: bool = False
+    @staticmethod
+    def __lerp(a, b, dt) -> float:  # noqa
+        return a + (b - a) * dt
 
-    def __create_particles_list(self, amount: int, start_position: Vector2, radius: int, color: tuple) -> None:
-        # if self._hit:
-        self._particles_list: list[Particle] = [Particle(start_position, radius, color, Vector2(1, 0)) for i in range(amount)]
-        for particle in self._particles_list:
-            particle.direction = Vector2(1, 0)
-            particle.x_offset = random.choice(particle.foo.get(tuple(particle.direction.xy))['x']) * random.random()
-            particle.x_offset = random.choice(particle.foo.get(tuple(particle.direction.xy))['x']) * random.random()
-        # self._hit: bool = False
+    def emit(self) -> None:
+        self.delete_particles()
 
-    #
-    def hit_old(self, amount: int, start_position: Vector2, radius: int, color: tuple[int, int, int]) -> None:
-        self._hit: bool = True
-        self.__create_particles_list(amount, start_position, radius, color)
-        for index, particle in enumerate(self._particles_list):
+        if self.particles:
 
-            particle.position.x += particle.x_offset
-            particle.x_offset - 1 if particle.x_offset < 0 else particle.x_offset + 1
-            particle.position.y += particle.y_offset
+            for particle in self.particles:
 
-            particle.radius -= 0.3
-            pg.draw.circle(self._surface, particle.color, particle.position, particle.radius)
-            if particle.radius <= 0:
-                self._particles_list.remove(particle)
+                delta: int = pg.time.get_ticks() - particle.start_time
 
-    def hit(self, amount: int, start_position: Vector2, radius: int, color: tuple[int, int, int]) -> None:
-        self._hit: bool = True
-        # self.__create_particles_list(amount, start_position, radius, color)
-        for index, particle in enumerate(self._particles_list):
-            print(particle.x_offset)
-            particle.position.x += particle.x_offset
-            # particle.x_offset - 1 if particle.x_offset < 0 else particle.x_offset + 1
-            particle.position.y += particle.y_offset
+                if delta < particle.duration:
+                    particle.pos_x = self.__lerp(
+                        particle.pos_vec.x, particle.dest_vec.x, delta / particle.duration
+                    )
 
-            # particle.radius -= 0.3
-            pg.draw.circle(self._surface, particle.color, particle.position, particle.radius)
-            if particle.radius <= 0:
-                self._particles_list.remove(particle)
+                    particle.pos_y = self.__lerp(
+                        particle.pos_vec.y, particle.dest_vec.y, delta / particle.duration
+                    )
 
-    def update(self, pos):
-        self.hit(200, pos, 4, (255, 0, 0))
+                    particle.radius = self.__lerp(
+                        particle.radius, particle.dest_radius, delta / particle.duration
+                    )
+
+                pg.draw.circle(
+                    particle.image, particle.color, (particle.pos_x, particle.pos_y), int(particle.radius)
+                )
+                particle.rect = particle.image.get_rect(center=(particle.pos_x, particle.pos_y))
+
+            # change particle lifetime
+            for particle in self.particles:
+                particle.lifetime -= random.random() * gSettings.PARTICLE_LIFETIME_COEFFICIENT
+
+    def add_particles(self) -> None:
+
+        particle_circle: ParticleBlood = ParticleBlood(
+            group=self.group,
+            pos_x=pg.mouse.get_pos()[0],
+            pos_y=pg.mouse.get_pos()[1],
+            radius=gSettings.BLOOD_PARTICLE_RADIUS,
+            direction_x=gSettings.BLOOD_PARTICLE_DIRECTION_X,
+            direction_y=gSettings.BLOOD_PARTICLE_DIRECTION_Y,
+            duration=gSettings.BLOOD_PARTICLE_DURATION,
+            start_time=pg.time.get_ticks()
+        )
+        self.particles: list[ParticleBlood] = [
+            particle_circle for _ in range(self.particles_amount)
+        ]
+        self.particles.append(particle_circle)
+
+    def delete_particles(self) -> None:
+        particle_copy: list[ParticleBlood] = [particle for particle in self.particles if particle.lifetime > 0]
+        self.particles: list[ParticleBlood] = particle_copy
+
+    def update(self, *args, **kwargs) -> None:
+        print(kwargs)
+        if kwargs['collide']:
+            self.add_particles()
+            self.emit()
+
