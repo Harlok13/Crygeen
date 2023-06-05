be3b3625e9654ad3da533acc61780a3cb0eabdea
harlok <holmik90@yandex.ru>
2023-06-06 01:17:59 +0400

    fix(blood_particle.py): normalize vector, bind particles to an entity

--- crygeen/game_process/particles/blood_particle.py
+++ crygeen/game_process/particles/blood_particle.py
@@ -1,9 +1,11 @@
 import random
+from typing import Any
 
 import pygame as pg
 from pygame import Surface, Vector2
 
 from crygeen.game_process.game_settings import gSettings, ParticleDirection, ParticleRadius
+from crygeen.utils.support import deprecated
 
 
 class ParticleBlood:
@@ -27,9 +29,12 @@ class ParticleBlood:
         self.radius: int = radius.start_radius  # TODO: ref title cuz its not a radius
         self.dest_radius: int = random.randint(radius.start_radius, radius.dest_radius)
 
-        self.direction_x: int = random.randint(*direction_x)  # TODO: normalize vector
+        self.direction_x: int = random.randint(*direction_x)
         self.direction_y: int = random.randint(*direction_y)
         self.dir_vec: Vector2 = pg.Vector2(self.direction_x, self.direction_y)
+
+        # normalize vector to circle
+        self.dir_vec.scale_to_length(min(self.dir_vec.length(), direction_x.positive))  # TODO: or make ellipse?
 
         self.dest_vec: Vector2 = pg.Vector2(self.pos_vec + self.dir_vec)
 
@@ -49,18 +54,20 @@ class ParticleBlood:
         self.offset_rect = self.rect
 
 
-class BloodParticlePlayer(pg.sprite.Sprite):
-    def __init__(self, group) -> None:
-        super().__init__(group)
+class BloodParticlePlayer:
+    def __init__(self, canvas) -> None:
         self.particles: list[ParticleBlood] = []
         self.particles_amount: int = gSettings.BLOOD_PARTICLE_AMOUNT
 
+        self.canvas: Surface = canvas
+
+        self.screen_size: tuple[float, float] = pg.display.get_window_size()
 
     @staticmethod
     def __lerp(a, b, dt) -> float:  # noqa
         return a + (b - a) * dt
 
-    def emit(self, canvas: Surface, player) -> None:
+    def emit(self, player) -> None:
         self.__delete_particles()
 
         if self.particles:
@@ -70,25 +77,19 @@ class BloodParticlePlayer(pg.sprite.Sprite):
                 delta: int = pg.time.get_ticks() - particle.start_time
 
                 if delta < particle.duration:
-                    particle.pos_x = self.__lerp(
-                        particle.pos_vec.x, particle.dest_vec.x, delta / particle.duration
-                    )
 
-                    particle.pos_y = self.__lerp(
-                        particle.pos_vec.y, particle.dest_vec.y, delta / particle.duration
+                    particle.pos_vec = self.__lerp(
+                        particle.pos_vec, particle.dest_vec, delta / particle.duration
                     )
 
                     particle.radius = self.__lerp(
                         particle.radius, particle.dest_radius, delta / particle.duration
                     )
 
-                particle.rect = particle.image.get_rect(center=(particle.pos_x, particle.pos_y))
-                # pg.draw.circle(
-                #     canvas, particle.color, (particle.rect.centerx - player.rect.centerx, particle.rect.centery - player.rect.centery), int(particle.radius)
-                # )
+                particle.rect = particle.image.get_rect(center=particle.pos_vec)
 
                 pg.draw.ellipse(
-                    canvas, particle.color,
+                    self.canvas, particle.color,
                     pg.Rect(particle.rect.centerx - player.rect.centerx, particle.rect.centery - player.rect.centery,
                             particle.radius, particle.radius // 2),
                 )
@@ -97,11 +98,11 @@ class BloodParticlePlayer(pg.sprite.Sprite):
             for particle in self.particles:
                 particle.lifetime -= random.random() * gSettings.PARTICLE_LIFETIME_COEFFICIENT
 
-    def add_particles(self) -> None:  # TODO: use as API
+    def add_particles(self, entity: Any) -> None:
 
         particle_circle: ParticleBlood = ParticleBlood(
-            pos_x=pg.mouse.get_pos()[0],
-            pos_y=pg.mouse.get_pos()[1],
+            pos_x=entity.rect.centerx + self.screen_size[0] / 2,  # TODO: must be hitbox
+            pos_y=entity.rect.centery + self.screen_size[1] / 2,
             radius=gSettings.BLOOD_PARTICLE_RADIUS,
             direction_x=gSettings.BLOOD_PARTICLE_DIRECTION_X,
             direction_y=gSettings.BLOOD_PARTICLE_DIRECTION_Y,
@@ -117,7 +118,8 @@ class BloodParticlePlayer(pg.sprite.Sprite):
         particle_copy: list[ParticleBlood] = [particle for particle in self.particles if particle.lifetime > 0]
         self.particles: list[ParticleBlood] = particle_copy
 
-    def update(self, canvas: Surface, player,  *args, **kwargs) -> None:
+    @deprecated
+    def update(self, player,  *args, **kwargs) -> None:
         self.add_particles()
-        self.emit(canvas, player)
+        self.emit(player)
 
